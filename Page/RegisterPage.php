06d6595b97886942d618c603955a2b43c5b07500
Srikarette <opkungkeke@gmail.com>
2022-10-20 16:42:45 +0700

    Register Page fixed

--- Page/RegisterPage.php
+++ Page/RegisterPage.php
@@ -18,30 +18,33 @@
 
         <form action="../php_data/type_select.php" method="post">
         <div class="box">
-            <div class="row">
+            <div class="column" id="select">
                 <div class="select">
                     <input type="hidden" id="sType" name="system_type" value="">
                     <input type="button" id="Farmer" data-hover1="เกษตรกร" onclick="displayUserSelectFunction1()"></button>
                     <input type="button" id="Organize" data-hover2="องค์กร" onclick="displayUserSelectFunction2()"></button>
                 </div>   
             </div>
-            <div class="row" id="TEXTBOX">
-                <h1>เพิ่งเริ่มต้นใช้งานใช่ไหม ?</h1>
-                <div class="part1">
-                    <p>เริ่มต้นด้วยการเลือกประเภทบัญชีที่เหมาะสมสำหรับคุณ</p>
-                    <p>เรามีทั้ง<p1 id="TextBold">บัญชีสำหรับเกษตรกร</p1> และ <p1 id="TextBold">บัญชีสำหรับองค์กร</p1> </p>
-                </div>
-                <div class="part2" >
-                    <p><p1 id="TextBold">คลิ๊กที่รูปภาพ</p1>  เพื่อทำการเลือกรูปแบบบัญชี </p>
-                    <div class="display" ><label id="demo"></label></div>
-                </div>
-                <div class="part3">
-                    <input type="Submit" class="Submit"><a id="RegSubmit" href="">ต่อไป</a></button>
-                    <input type="button" class="Submit" ><a href="/Page/LoginPage.html">กลับ</a></button>
-                </div>
-           </div>
-        </div>
+            <div class="column" id="textbox">
+                    
+                    <div class="part1">
+                    <h1>เพิ่งเริ่มต้นใช้งานใช่ไหม ?</h1>
+                            <p>เริ่มต้นด้วยการเลือกประเภทบัญชีที่เหมาะสมสำหรับคุณ</p>
+                            <p>เรามีทั้ง<p1 id="TextBold"> บัญชีสำหรับเกษตรกร</p1> และ <p1 id="TextBold"> บัญชีสำหรับองค์กร</p1> </p>
+                    </div>
+                    <div class="part2" >
+                        <p><p1 id="TextBold">คลิ๊กที่รูปภาพ</p1>  เพื่อทำการเลือกรูปแบบบัญชี </p>
+                        <div class="display" ><label id="demo"></label></div>
+                    </div>
+                    <div class="part3">
+                         <a id="RegSubmit" href=""> <input type="Submit" class="Submit"></button></a>
+                    </div>
+                    <div class="part3">
+                         <a href="/Page/LoginPage.html"><input type="button" class="Submit" value="กลับ"></button></a> 
+                    </div>
+            </div>
         </form>
+        
 
        <script>
         function displayUserSelectFunction1() {
